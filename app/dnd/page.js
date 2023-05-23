0b73a24fedd02474ccd2631e19dc035f38973dd0
saurabhsigna <131267391+saurabhsigna@users.noreply.github.com>
2023-05-23 05:12:12 +0000

    first commit (#1)

--- app/dnd/page.js
+++ app/dnd/page.js
@@ -1,2 +1,18 @@
 "use client"
 import React from 'react'
+import {DragDropContext,Droppable,Draggable} from 'react-beautiful-dnd'
+import uuid from 'uuid'
+
+const itemsFromBackend = [
+    {id:uuid(),content:"first task"}
+]
+export default function App(){
+    return (
+        <div className='h-screen max-w-screen flex items-center justify-center'>
+            <DragDropContext onDropEnd={result=>console.log(result)} >
+                
+                
+            </DragDropContext>
+            </div>
+    )
+}
