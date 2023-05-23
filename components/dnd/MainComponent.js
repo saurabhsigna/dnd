73fe5885250660fd7736e98476f3dd59f6dbcbaf
saurabhsigna <131267391+saurabhsigna@users.noreply.github.com>
2023-05-23 06:20:40 +0000

    update 3 files
    * first commit
    
    * Update 3 files

---
+++ components/dnd/MainComponent.js
@@ -0,0 +1,117 @@
+"use client";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuid } from "uuid";
+const itemsFromBackend = [
+  { id: uuid(), content: "first task" },
+  { id: uuid(), content: "second task" },
+  { id: uuid(), content: "third task" },
+  { id: uuid(), content: "fourth task" },
+  { id: uuid(), content: "fifth task" },
+  { id: uuid(), content: "sixth task" },
+  { id: uuid(), content: "seventh task" },
+  { id: uuid(), content: "eighth task" },
+  { id: uuid(), content: "ninth task" },
+  { id: uuid(), content: "tenth task" },
+];
+const columnsFromBackend = {
+  [uuid()]: {
+    name: "TODO",
+    items: itemsFromBackend,
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
+
+export default function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+  return (
+    <div className="h-screen max-w-screen flex items-center justify-center">
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
+        {Object.entries(columns).map(([id, column]) => (
+          <Droppable droppableId={id}>
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{
+                  width: 250,
+                  padding: 4,
+                  backgroundColor: snapshot.isDraggingOver
+                    ? "lightblue"
+                    : "lightgray",
+                  minHeight: 500,
+                }}
+              >
+                {column.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            userSelect: "none",
+                            padding: 16,
+                            margin: "0 0 8px 0",
+                            minHeight: "50px",
+                            backgroundColor: snapshot.isDragging
+                              ? "#263B4A"
+                              : "#456C86",
+                            color: "white",
+                            ...provided.draggableProps.style,
+                          }}
+                        >
+                          {item.content}
+                        </div>
+                      );
+                    }}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </DragDropContext>
+    </div>
+  );
+}
